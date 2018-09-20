77c81539f57a12cfaf965aff72c193bb97acd6b1
mark7dev <mcamacho_ce7@live.com.mx>
2018-09-20 00:57:16 -0500

    POST User with condition

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -30,34 +30,54 @@ const Controller = {
     //POST***
     create: (request, response) => {
 
-        //Encrypting password
-        bcrypt.hash(request.body.password, 10, (error, hash) => {
-            if (error) {
-                return response
-                    .status(500)
-                    .json({
-                        message: error
-                    })
-            }
+        //Checking email don't repeat
+        User
+            .find({
+                email: request.body.email
+            })
+            .exec()
+            .then(users => {
+                if (users.length < 1) {
+  
+                    //save new user
+                    //Encrypting password
+                    bcrypt.hash(request.body.password, 10, (error, hash) => {
+                        if (error) {
+                          return response
+                            .status(500)
+                            .json({
+                              message: error
+                            });
+                        }
+            
 
-            //Creating data
-            const newUser = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: request.body.email,
-                password: hash
-            });
+                        //Creating data
+                        const newUser = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: request.body.email,
+                            password: hash
+                        });
+              
 
-            //Saving data
-            newUser
-                .save()
-                .then(saved => {
+                        //Saving data
+                        newUser
+                        .save()
+                        .then(saved => {
+                          response
+                            .status(201)
+                            .json({
+                              message: 'User created successfully.'
+                            });
+                        });
+                    });           
+                } else {
                     response
-                        .status(201)
+                        .status(422)
                         .json({
-                            message: 'User created succesfully.'
-                        })
-                })
-        }); 
+                            message: 'User already exists.'
+                        });
+                }
+            })
     }
 };
 
